f7c71bf114f9acfff3400bd818698619c833765b
chris.lee <Savier!4349>
2021-09-22 20:09:45 +1000

    CSS practice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   const test = true;
   const style = {
-    backgroundColor: 'white',
-    color: 'aqua',
-    padding: 30
-  }
+    backgroundColor: "white",
+    color: "aqua",
+    padding: 30,
+  };
   return (
     <div style={style}>
       {test === true ? (
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Test Mode
-            </p>
+            <p>Test Mode</p>
             <a
               className="App-link"
               href="https://reactjs.org"
@@ -31,9 +29,7 @@ function App() {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Production Mode
-            </p>
+            <p>Production Mode</p>
             <a
               className="App-link"
               href="https://reactjs.org"
