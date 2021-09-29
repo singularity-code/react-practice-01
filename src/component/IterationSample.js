42dbe7bb2129f3d2b2ba231298b6af7b54d3022c
D.L <neoxrock@gmail.com>
2021-09-29 21:35:32 +1000

    Dynamic component

--- src/component/IterationSample.js
+++ src/component/IterationSample.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 function IterationSample() {
-  const names = ["snow", "ice", "fire", "wind"];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+  const [names, setNames] = useState([
+    { id: 1, text: "snow" },
+    { id: 2, text: "ice" },
+    { id: 3, text: "fire" },
+    { id: 4, text: "wind" },
+  ]);
+  const [inputText, setInpuText] = useState("");
+  const [nextId, setNextId] = useState(5);
+  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
+
+  const onChange = (e) => setInpuText(e.target.value);
+  const onClick = () => {
+    const nextNames = names.concat({
+      id: nextId,
+      text: inputText,
+    });
+    setNextId(nextId + 1);
+    setNames(nextNames);
+    setInpuText("");
+  };
   return (
     <div>
+      <input value={inputText} onChange={onChange}></input>
+      <button onClick={onClick}>INSERT</button>
       <ul>{nameList}</ul>
-      
     </div>
   );
 }
