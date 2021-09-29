5b60bb5f9f7018ccfe145623edc7bd4ddabf0618
chris.lee <Savier!4349>
2021-09-29 21:00:02 +1000

    Component iteration

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import NewComponent from "./component/NewComponent";
-import Say from "./component/Say";
-import EventPractice from "./component/EventPractice";
-import CreateRef from "./component/CreateRef";
 import ScrollBox from "./component/ScrollBox";
+import IterationSample from "./component/IterationSample";
 
 class App extends Component {
   render() {
@@ -21,24 +19,14 @@ class App extends Component {
           <div className="App">
             <header className="App-header">
               <p>Test Mode</p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
               <NewComponent name={"Correct Name"} favNum={100}>
                 <div>This is a child</div>
               </NewComponent>
-              <Say></Say>
-              <EventPractice></EventPractice>
-              <CreateRef></CreateRef>
               <ScrollBox ref={(ref) => (this.scrollBox = ref)}></ScrollBox>
               <button onClick={() => this.scrollBox.scrollToBottom()}>
                 Go to Bottom
               </button>
+              <IterationSample></IterationSample>
             </header>
           </div>
         ) : (
---
+++ src/component/IterationSample.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function IterationSample() {
+  const names = ["snow", "ice", "fire", "wind"];
+  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+  return (
+    <div>
+      <ul>{nameList}</ul>
+    </div>
+  );
+}
+
+export default IterationSample;
